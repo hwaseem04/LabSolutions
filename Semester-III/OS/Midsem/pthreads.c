b22999b1d154aad52e9f1dd0092cf90cc4c78c8a
Muhammad Waseem <muhammadwaseem@Muhammads-MacBook-Pro.local>
2022-09-10 15:10:12 +0530

    new file:   Semester-III/OS/Midsem/pthreads

---
+++ Semester-III/OS/Midsem/pthreads.c
@@ -0,0 +1,93 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <math.h>
+
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+int GSUM = 0;
+
+typedef struct Elem{
+    int id;
+    int start;
+    int end;
+    int* arr;
+}Elem;
+
+int*  fill_array(int);
+void* local_sum(void*);
+
+// T : Number of threads
+// N : Number of Elements
+// j : jth Thread
+int NumElems_to_Thread(int N, int j, int T){
+    return (floor(N * (j + 1)/T) - floor(N * j/T));
+}
+
+int main(void){
+    int n;
+    printf("Enter Elements in array : ");
+    scanf("%i", &n);
+
+    //Elem* Struct_arr[5];
+    Elem** Struct_arr = (Elem**)malloc(sizeof(Elem*) * 5);
+
+    for (int i = 0; i < 5; i++){
+        Struct_arr[i] = (Elem*)malloc(sizeof(Elem));
+    }
+
+    int map[5];
+    for (int i = 0; i < 5; i++)
+        map[i] = NumElems_to_Thread(n, i, 5);
+
+    pthread_t Thread_Arr[5];
+    int* main_arr = fill_array(n);
+
+    printf("\n");
+
+    int count = 0;
+    for(int i = 0; i < 5; i++){
+        Struct_arr[i] -> id = i + 1;
+        Struct_arr[i] -> start = count;
+        count += map[i];
+        Struct_arr[i] -> end = count - 1;
+        Struct_arr[i] -> arr = main_arr;
+    }
+
+    for (int i = 0; i < 5; i++){
+        pthread_create(&Thread_Arr[i], NULL, local_sum, (void*)Struct_arr[i]);
+    }
+    for (int i = 0; i < 5; i++){
+        pthread_join(Thread_Arr[i], NULL);
+    }
+    printf("Global SUM : %i\n", GSUM);
+
+    free(Struct_arr);
+    free(main_arr);
+
+    return 0;
+}
+
+void* local_sum(void* S){
+    pthread_mutex_lock( &m1 );
+
+    Elem* S_local = (Elem*)S;
+    int local_sum = 0;
+    int range = S_local -> end - S_local -> start;
+    for (int i = S_local -> start; i <= S_local -> end; i++){
+        local_sum += S_local -> arr[i];
+        printf("Thread id : %i, local add : %i\n", S_local -> id, S_local ->arr[i]);
+    }
+    
+    printf("Local Sum : %i\n\n", local_sum);
+    GSUM += local_sum;
+    pthread_mutex_unlock( &m1 );
+    return NULL;
+}
+
+int* fill_array(int n){
+    int* arr = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++){
+        *(arr + i) = i + 1;
+    }
+    return arr;
+}
--- Semester-III/OS/ex5/pc.c
+++ Semester-III/OS/ex5/pc.c
@@ -5,7 +5,7 @@
 
 int buffer[MAX];
 int front = -1, rear = -1;
-int counter = 0;
+//int counter = 0;
 int flag = 0;
 int flag2 = 1;
 
@@ -52,48 +52,56 @@ int dequeue(){
 }
 
 int main(void){
-    pthread_t p1, c1;
+    pthread_t p1, c1, p2, c2;
 
     pthread_create( &p1, NULL, produce, NULL );
     pthread_create( &c1, NULL, consume, NULL );
+    pthread_create( &p2, NULL, produce, NULL );
+    pthread_create( &c2, NULL, consume, NULL );
 
-    
     pthread_join(p1, NULL);
     pthread_join(c1, NULL);
+    pthread_join(p2, NULL);
+    pthread_join(c2, NULL);
 
     return 0;
 }
 
-
-
 void* produce(void* dummy){
+    int counter = 0;
+    //while (counter < 10){
+    pthread_mutex_lock( &mutex1 );
+    
+    if (isfull()) 
+        pthread_cond_wait( &cond1, &mutex1 );
 
-    while (1){
-        pthread_mutex_lock( &mutex1 );
-        
-        if (isfull()) 
-            pthread_cond_wait( &cond1, &mutex1 );
-
+    if (counter < 10)
         enqueue();
-        printf("Thread id : %ld produced %i\n", pthread_self() , rear);
+    printf("Thread id : %ld produced %i\n", pthread_self() , rear);
 
-        pthread_mutex_unlock( &mutex1 );
+    counter ++;
 
-        pthread_cond_signal( &cond1 );
-    }
+    pthread_mutex_unlock( &mutex1 );
+
+    pthread_cond_signal( &cond1 );
+    //}
 }
 
 void* consume(void* dummy){
-    while(1){
-        pthread_mutex_lock( &mutex1 );
+    int counter = 0;
+    //while(counter < 10){
+    pthread_mutex_lock( &mutex1 );
 
-        if (isempty())
-            pthread_cond_wait( &cond1, &mutex1 );
-        
+    if (isempty())
+        pthread_cond_wait( &cond1, &mutex1 );
+    
+    if (counter < 10)
         printf("Thread id : %ld consumed %i\n", pthread_self() , dequeue());
 
-        pthread_mutex_unlock( &mutex1 );
+    counter ++;
 
-        pthread_cond_signal( &cond1 );
-    }
+    pthread_mutex_unlock( &mutex1 );
+
+    pthread_cond_signal( &cond1 );
+    //}
 }
